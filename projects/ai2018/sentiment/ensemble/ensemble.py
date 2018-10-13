fix sentiment evaluate bug with gezi.softmax default dim change to -1

projects/ai2018/sentiment/ensemble/ensemble.py
@@ -89,16 +89,16 @@ def calc_f1_alls(labels, predicts):
 class_weights = np.load('/home/gezi/temp/ai2018/sentiment/class_weights.npy')
 #print('class_weights', class_weights)
 
-# for i in range(len(class_weights)):
-#   for j in range(4):
-#     #class_weights[i][j] = math.log(class_weights[i][j])
-#     #class_weights[i] = gezi.softmax(class_weights[i])
-#     #class_weights[i][j] +=  math.sqrt(class_weights[i][j])
-#     #class_weights[i][j] += 0.
-#     #class_weights[i][j] = math.sqrt(class_weights[i][j])
-#     #x = 1./(1 - class_weights[i][j])
-#     #class_weights[i][j] = x
-#     #class_weights[i][j] = x * x
+for i in range(len(class_weights)):
+  for j in range(4):
+    #class_weights[i][j] = math.log(class_weights[i][j])
+    #class_weights[i] = gezi.softmax(class_weights[i])
+    #class_weights[i][j] +=  math.sqrt(class_weights[i][j])
+    #class_weights[i][j] += 0.
+    #class_weights[i][j] = math.sqrt(class_weights[i][j])
+    x = class_weights[i][j]
+    class_weights[i][j] = x
+    #class_weights[i][j] = x * x
 
 def to_predict(logits, weights=None, is_single=False):
   ## DO NOT divde !!

projects/common/lm/dataset.py
@@ -117,6 +117,7 @@ class Dataset(melt.tfrecords.Dataset):
 
           def reset(self):
             self.i = 0
+            np.random.shuffle(self.ori_data)
             self.data = np.concatenate(self.ori_data)
             data_len = len(self.data)
             batch_len = data_len // batch_size
@@ -136,7 +137,6 @@ class Dataset(melt.tfrecords.Dataset):
               self.i += bptt
               return x, y
             else:
-              np.random.shuffle(self.ori_data)
               self.reset()
               raise StopIteration()
 

utils/gezi/util.py
@@ -78,7 +78,7 @@ def cosine(a, b):
   from numpy.linalg import norm
   return dot(a, b)/(norm(a)*norm(b))
 
-def softmax(x, axis=0):
+def softmax(x, axis=-1):
     mx = np.amax(x, axis=axis, keepdims=True)
     x_exp = np.exp(x - mx)
     x_sum = np.sum(x_exp, axis=axis, keepdims=True)